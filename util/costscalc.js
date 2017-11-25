da1fbfb55834e155b261c29564ef5feefa2f7d06
voldemarich <voldemarich@voldkraft.com>
2017-11-25 22:28:59 +0200

    added cost calculation

--- util/costscalc.js
+++ util/costscalc.js
@@ -1,3 +1,21 @@
 /**
  * Created by voldemarich on 25.11.2017.
  */
+//** constants **//
+const per_km = 0.5;
+const per_kg = 1.6;
+const liquid_coefficient = 1.2;
+const gas_coefficient = 1.5;
+const fragility = 1.1;
+const speciality = 1.3;
+
+exports.calculate = function(order_json)
+{
+  sum = 0;
+  sum = order_json.distance*per_km + order_json.weight*per_kg;
+  if(order_json.goods_type === "liquid") sum *= liquid_coefficient;
+  if(order_json.goods_type === "gas") sum *= gas_coefficient;
+  if(order_json.shake_level !== "no_importance") sum *= fragility;
+  if(order_json.goods_special !== "not_special") sum *= speciality;
+  return sum;
+};
